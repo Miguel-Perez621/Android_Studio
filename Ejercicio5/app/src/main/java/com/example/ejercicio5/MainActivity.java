d2e55d3da6ca64e50b6dc32550efea980c8756a7
Miguel <mpc0080@alu.medac.es>
2025-10-10 14:28:39 +0200

    ejercicio5 casi hecho

---
+++ Ejercicio5/app/src/main/java/com/example/ejercicio5/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.ejercicio5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text_mundo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
